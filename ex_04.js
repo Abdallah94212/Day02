d32cb6ffcf384a022e56363582eaf62591ba4046
Abdallah94212 <abdallah.borji@epitech.digital>
2024-10-01 13:54:24 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,16 +1,12 @@
 let a = 2; 
 let b = 4;
 let action = "sub";
-
-
 function addition() {
   return a + b;
 }
-
 function subtraction() {
   return a - b;
 }
-
 function calculator() {
   if (action === "add") {
     return addition(a, b);
