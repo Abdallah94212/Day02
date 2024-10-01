cc98de77fa6801ad41582a9b0bc78fddff5a4921
Abdallah94212 <abdallah.borji@epitech.digital>
2024-10-01 11:54:53 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,17 @@
+function changebackground() {
+
+{let magicNumber = 1
+  if (magicNumber === 1) 
+    paintYellowRectangle();
+  }
+   { var magicNumber = 2;
+  if (magicNumber === 2) 
+    paintGreenRectangle();
+  }
+   { let magicNumber = 3;
+  if (magicNumber === 3) 
+    paintRedRectangle();
+  }
+}
+
+changebackground();
